98ead057574bb33d59c3fd1cdb958b24f9f177c0
Zaela Rose <zaela.rose@tutanota.com>
2023-12-15 14:56:05 -0600

    Add animation between multiple screens (Hero widget)

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -38,12 +38,15 @@ class MealItem extends StatelessWidget {
         // Stack widget literally stacks widgets on top of each other
         child: Stack(
           children: [
-            FadeInImage(
-              placeholder: MemoryImage(kTransparentImage),
-              image: NetworkImage(meal.imageUrl),
-              fit: BoxFit.cover,
-              height: 200,
-              width: double.infinity,
+            Hero(
+              tag: meal.id,
+              child: FadeInImage(
+                placeholder: MemoryImage(kTransparentImage),
+                image: NetworkImage(meal.imageUrl),
+                fit: BoxFit.cover,
+                height: 200,
+                width: double.infinity,
+              ),
             ),
             Positioned(
               bottom: 0,
